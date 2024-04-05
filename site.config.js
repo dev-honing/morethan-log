cf75abe666367bfb2251541d85060a4bb2e3a86e
Ho Nyeong <dev.honing@gmail.com>
2024-04-05 20:19:50 +0900

    Fix: 프로필 이미지 수정

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Honing",
-    image: "https://s3.ap-northeast-2.amazonaws.com/bhn1997.com/turtle.jpg",
+    image: "/avatar.svg",
     role: "풀스택 개발자",
     bio: "새로운 것을 배워 개발에 적용하는 것이 행복한 주니어 개발자",
     email: "dev.honing@gmail.com",
